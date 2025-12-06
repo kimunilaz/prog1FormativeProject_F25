5da88c2fa419e77640bf06c82b882f84326464cc
kimunilaz <kzhakata7@gmail.com>
2025-12-07 03:29:31 +0400

    BudgetTracker Class added with add-expense and add-income methods

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ class Transaction:
     def __str__(self):
         return f"{self.date} | {self.type.capitalize():8} | ${self.amount:>..2f} | {self.category:15} | {self.description}"
 
+#Extract YYYY-MM format from date
+    def get_month(self):
+
+        return self.date[:7]
+
 
 
 
@@ -30,7 +35,24 @@ class Expense(Transaction):
     def __init__(self, date, amount, category, description):
         super().__init__(date, amount, category, description, 'expense')# inheriting attributes from transaction class(parent class)
 
+
+# BudgetTracker class to manage all transactions
+class BudgetTracker:
+    def __init__(self):
+        self.transactions = [] #creates an empty list where transactions are to be stored
+
+    def add_income(self, date, amount, category, description):
+        myincome = Income(date, amount, category, description) # add an income transaction
+        self.transactions.append(myincome)
+        print(f"✓ Income of ${amount} added successfully!")
+
+    def add_expense(self, date, amount, category, description):
+        expense = Expense(date, amount, category, description) #add an expense transaction
+        self.transactions.append(expense)
+        print(f"✓ Expense of ${amount} added successfully!")
+
 def menu():
+    Tracker = BudgetTracker() # create an object to store transactions
 
     while True:
         print("\n")
@@ -38,11 +60,32 @@ def menu():
         print("\n")
         print("1) Add income")
         print("2) Add expense")
-        print("3) List transactions")
-        print("4) Filter transactions")
-        print("5) Show summary")
-        print("0) Exit")
+        #print("3) List transactions")
+        #print("4) Filter transactions")
+        #print("5) Show summary")
+        #print("0) Exit")
         print("\n")
-        break
 
+        choice = input("Select an option: ").strip()
+        if choice == "1":
+            date = (input("Enter a date: "))
+            amount = input("Enter a amount: ").strip()
+            category = input("Enter a category: ").strip()
+            description = input("Enter a description: ").strip()
+
+            Tracker.add_income(date, amount, category, description)
+
+
+        if choice == "2":
+            date = input("Enter a date: ").strip()
+            amount = input("Enter a amount: ").strip()
+            category = input("Enter a category: ").strip()
+            description = input("Enter a description: ").strip()
+
+            Tracker.add_expense(date, amount, category, description)
+
+
+
+        break
+ # TESTING
 print(menu())
